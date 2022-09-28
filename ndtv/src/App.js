90604b319afdd0c87ebfce4501d09fd469a39ca2
nilamkhose2001 <nilamkhose2001@gmail.com>
2022-09-28 20:10:08 +0530

    add

---
+++ ndtv/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from "./Components/Navbar"
+import AllRoutes from './Components/AllRoutes';
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ ndtv/src/Components/AllRoutes.jsx
@@ -0,0 +1,13 @@
+//import { Route,Routes } from "react-router-dom";
+
+export default function AllRoutes()
+{
+    return <>
+   
+        {/* <Routes>
+        
+        </Routes>
+  */}
+    
+    </>
+}
